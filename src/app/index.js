7def9b58e831112444e8733d8e6901ab348f81fb
Renne Rocha <rrocha@mwneo.com>
2017-01-19 22:34:25 -0200

    first usable version of system

---
+++ src/app/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+export default class App extends Component {
+  render() {
+    return <div>Beer Styles Guide</div>
+  }
+}
+
+ReactDOM.render(
+  <App />
+  , document.querySelector('.container'));
